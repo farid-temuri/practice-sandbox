2480b179c23959b0a7ef3d1fd5d25f847283c046
Farid <99648043+farid-temuri-lofty@users.noreply.github.com>
2022-12-23 11:10:43 +0300

    wip

--- cjs-modules/index.js
+++
@@ -1,7 +0,0 @@
-const { characters, stealRing } = require('./characters.js')
-
-let myChar = characters
-
-myChar = stealRing(characters, 'Frodo')
-
-console.log(characters);
---
+++ test/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+
+    
+    const a = ref('test string')
+  }
+})
+</script>
+
+<template>
+  <div>
+    {{ a }}
+  </div>
+</template>
